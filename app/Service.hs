81469ddcecf0061759332c7cbd2a0ddf6dfc6e9d
John Ky <newhoggy@gmail.com>
2018-02-16 14:17:46 +1100

    Move Codec related functions to Codec module

--- app/Service.hs
+++ app/Service.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections       #-}
 
 module Service
   ( handleStream
@@ -8,17 +6,13 @@ module Service
   ) where
 
 import App
+import App.Codec
 import App.ToSha256Text
 import Conduit
-import Control.Arrow                        (left)
-import Control.Lens                         ((+=), (<&>))
-import Control.Monad.Catch                  (MonadThrow)
-import Control.Monad.IO.Class               (MonadIO)
-import Data.Aeson                           (object, (.=))
+import Control.Lens                         ((+=))
 import Data.Avro.Schema                     ()
 import Data.Avro.Types                      ()
 import Data.ByteString                      (ByteString)
-import Data.ByteString.Lazy                 (fromStrict)
 import Data.Monoid                          ((<>))
 import HaskellWorks.Data.Conduit.Combinator
 import Kafka.Avro
@@ -26,14 +20,9 @@ import Kafka.Conduit.Source
 
 import qualified Data.Aeson              as J
 import qualified Data.Aeson.Text         as JT
-import qualified Data.Avro.Decode        as A
-import qualified Data.Avro.Schema        as A
-import qualified Data.Avro.Types         as A
 import qualified Data.ByteString         as BS
-import qualified Data.ByteString.Base16  as Base16
 import qualified Data.ByteString.Lazy    as LBS
 import qualified Data.Text               as T
-import qualified Data.Text.Encoding      as T
 import qualified Data.Text.Lazy.Encoding as LT
 import qualified System.Directory        as D
 
@@ -49,36 +38,10 @@ handleStream sr fp =
   .| effectC (const (writeCount += 1))
   .| mapC (const ())
 
-decodeMessage :: (MonadIO m, MonadThrow m)
-              => SchemaRegistry
-              -> ConsumerRecord (Maybe ByteString) (Maybe ByteString)
-              -> m (ConsumerRecord (Maybe ByteString) J.Value)
-decodeMessage sr msg = do
-  let (_, v) = (crKey msg, crValue msg)
-  res <- traverse decodeAvroMessage v
-  let payload = object [ "offset"        .= unOffset (crOffset msg)
-                       , "timestamp"     .= unTimeStamp (crTimestamp msg)
-                       , "partitionId"   .= unPartitionId (crPartition msg)
-                       , "key"           .= (encodeBs <$> crKey msg)
-                       , "valueSchemaId" .= (unSchemaId . fst <$> res)
-                       , "value"         .= (snd <$> res)
-                       ]
-  return $ const payload <$> msg
-  where
-    decodeAvroMessage bs = decodeAvro sr (fromStrict bs) >>= throwAs DecodeErr
-    unPartitionId (PartitionId v) = v
-    unSchemaId (SchemaId v) = v
-    unOffset (Offset v) = v
-    unTimeStamp = \case
-      CreateTime (Millis m)    -> Just (object ["type" .= J.String "CreatedTime",   "value" .= m])
-      LogAppendTime (Millis m) -> Just (object ["type" .= J.String "LogAppendTime", "value" .= m])
-      NoTimestamp              -> Nothing
-    encodeBs bs = "\\u" <> T.decodeUtf8 (Base16.encode bs)
-
-writeDecodedMessage :: MonadIO m => FilePath -> ConsumerRecord (Maybe BS.ByteString) J.Value -> m ()
-writeDecodedMessage parentPath msg = liftIO $ do
-  D.createDirectoryIfMissing True dirPath
-  LBS.appendFile (dirPath <> "/" <> show partitionId) (LT.encodeUtf8 (JT.encodeToLazyText (crValue msg)))
+writeDecodedMessage :: MonadApp m => FilePath -> ConsumerRecord (Maybe BS.ByteString) J.Value -> m ()
+writeDecodedMessage parentPath msg = do
+  liftIO $ D.createDirectoryIfMissing True dirPath
+  liftIO $ LBS.appendFile (dirPath <> "/" <> show partitionId) (LT.encodeUtf8 (JT.encodeToLazyText (crValue msg)))
   where TopicName topicName     = crTopic msg
         PartitionId partitionId = crPartition msg
         dirPath                 = parentPath <> "/" <> topicName
@@ -91,18 +54,3 @@ mkObjectKey :: TopicName -> Timestamp -> PartitionId -> ByteString -> T.Text
 mkObjectKey (TopicName topicName) currentTime (PartitionId partitionId) bs = toSha256Text path <> "/" <> path
     where contentHash = toSha256Text bs
           path        = T.pack topicName <> "/" <> T.pack (show partitionId) <> "/" <> T.pack (show currentTime) <> "-" <> contentHash <> ".json"
-
-decodeAvro :: MonadIO m
-        => SchemaRegistry
-        -> LBS.ByteString
-        -> m (Either DecodeError (SchemaId, A.Value A.Type))
-decodeAvro sr bs =
-  case schemaData of
-    Left err -> return $ Left err
-    Right (sid, payload) -> do
-      res <- left DecodeRegistryError <$> loadSchema sr sid
-      return $ res >>= decode payload <&> (sid,)
-  where
-    schemaData = maybe (Left BadPayloadNoSchemaId) Right (extractSchemaId bs)
-    decode p s = left (DecodeError s) (A.decodeAvro s p)
-
