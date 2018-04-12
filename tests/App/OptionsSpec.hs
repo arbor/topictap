e7e43d990cf9736e521e585c4871a60c14165b62
John Ky <newhoggy@gmail.com>
2018-04-12 16:00:13 +1000

    Convert tests to use hedgehog

--- tests/App/OptionsSpec.hs
+++ tests/App/OptionsSpec.hs
@@ -5,12 +5,13 @@ module App.OptionsSpec
   ) where
 
 import App.Options
-
+import HaskellWorks.Hspec.Hedgehog
+import Hedgehog
 import Test.Hspec
 
 {-# ANN module ("HLint: ignore Redundant do"  :: String) #-}
 
 spec :: Spec
 spec = describe "App.OptionsSpec" $ do
-    it "should parse tags" $
-      string2Tags "club_name:indica,deploy_id:manual" `shouldBe` [StatsTag ("club_name", "indica"), StatsTag ("deploy_id", "manual")]
+    it "should parse tags" $ requireTest $ do
+      string2Tags "club_name:indica,deploy_id:manual" === [StatsTag ("club_name", "indica"), StatsTag ("deploy_id", "manual")]
